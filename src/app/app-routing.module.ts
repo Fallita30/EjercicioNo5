3540b5f82704a8915943227c1e0764ce29d5b121
EjercicioNo5 <edwinfalla30@outlook.es>
2022-09-23 16:44:23 -0500

    Actualización

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CharactersComponent } from './characters/characters.component';
 
 const routes: Routes = [
+  //caracterización de las rutas para las opciones de cada comics
   {
     path: '', redirectTo: '/characters', pathMatch: 'full'
   },
